35f2b57de2fc9d461035cb40e1f7981f8aa5df52
khanimranegipto <khanimranegipto@gmail.com>
2025-11-19 10:40:58 +0400

    completed the code for activities 8 and 9.

--- 08-SimpleSearch/08-SimpleSearch.py
+++ 08-SimpleSearch/08-SimpleSearch.py
@@ -1,2 +1,10 @@
-names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+while True: # added this here to restart the code in case the user made an error.
+ names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] # initialisin the list
+ userinput  = input("Search for a name here: ") # asks for the user's input
 
+ if userinput in names: # checks if the name that the user put in is in the list.
+    print(userinput + " is in the list.")
+    break 
+ else: 
+    print("That name is not in the list.")
+    continue
